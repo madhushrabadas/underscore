88db82075b067afc46a7001ec9d5bf4fa834b25b
Tuli Das <madhushraba21@gmail.com>
2023-11-25 14:09:18 +0530

    ;

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -21,7 +21,7 @@ const Body = () => {
 
       <div className="right">
         <div className="content">
-          <div className="heading">ASgaard Sofa</div>
+          <div className="heading">Asgaard Sofa</div>
           <div className="price">Rs. 250,000.00</div>
           <div className="review">
             <img src={star} alt="ddfvd" />
@@ -42,10 +42,50 @@ const Body = () => {
           <div className="size">
             <span>size</span>
             <div className="box">
-              <div className="b1"></div>
-              <div className="b1"></div>
-              <div className="b1"></div>
+              <div className="b1">L</div>
+              <div className="b1">XL</div>
+              <div className="b1">XS</div>
             </div>
+          </div>
+          <div className="color">
+            <span>color</span>
+            <div className="box">
+              <div className="b2"> </div>
+              <div className="b3"> </div>
+              <div className="b4"> </div>
+            </div>
+          </div>
+
+          <div className="addtocart">
+            <div className="rect1">
+              <span>-</span>1 <span>+</span>
+            </div>
+            <div className="rect2">Add To Cart</div>
+            <div className="rect3">+ compare</div>
+          </div>
+        </div>
+
+        <hr />
+        <div className="btm">
+          <div className="label">
+            <div className="lf">SKU </div>
+            <span>:</span>
+            <div className="rt">SS001</div>
+          </div>
+          <div className="label">
+            <div className="lf">Category </div>
+            <span>:</span>
+            <div className="rt">sofas</div>
+          </div>
+          <div className="label">
+            <div className="lf">Tags </div>
+            <span>:</span>
+            <div className="rt">Sofa, Chair, Home, Shop</div>
+          </div>
+          <div className="label">
+            <div className="lf">share </div>
+            <span>:</span>
+            <div className="rt">icons</div>
           </div>
         </div>
       </div>
